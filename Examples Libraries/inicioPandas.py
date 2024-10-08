8ba7faa231d1de3c311ef43b070ac70bc5b40416
Gustavo Linares Villegas <guslinaresv@gmail.com>
2024-10-07 22:31:24 -0600

    Add: numpy dataframes

--- Examples Libraries/inicioPandas.py
+++ Examples Libraries/inicioPandas.py
@@ -44,3 +44,66 @@ print(dataframe['leche'])
 #Valor de una celda
 print('Valor de una celda: ')
 print(dataframe.loc['tienda2']['carne'])
+
+#Nueva columna
+dataframe["lechuga"] = 40
+print('Nueva columna: ')
+print(dataframe)
+dataframe["total"] = dataframe['leche'] + dataframe['huevos'] + dataframe['pan'] + dataframe['carne'] + dataframe['lechuga']
+print('Total: ')
+print(dataframe)
+
+#Nueva fila
+dataframe.loc['tienda5'] = [23, 45, 23, 43, 23, 157]
+print('Nueva fila: ')
+print(dataframe)
+
+# eliminar colujmna
+dataframe = dataframe.drop('huevos', axis=1) #Se utiliza axis=1 para indicar que es una columna
+print('Eliminar columna: ')
+print(dataframe)
+
+#condificiones en dataframes
+print('Condiciones en dataframes: ')
+condicion = dataframe > 200
+print(dataframe[condicion])
+
+#Añadiendo inidices
+newColumn = [1,2,3,4,5]
+dataframe['indice'] = newColumn #Agregando inidce
+print('Añadiendo indices: ')
+dataframe = dataframe.set_index('indice') #Cambiando el datatframe
+print(dataframe)
+
+#Eliminando indice
+print('Eliminando indice: ')
+dataframe = dataframe.reset_index() #Eliminando el indice
+print(dataframe)
+
+#Union de dataframes
+data1 = {'A': [1,2,3], 'B': [4,5,6]}
+data2 = {'C': [7,8,9], 'D': [10,11,12]}
+dataframe1 = pd.DataFrame(data1)
+dataframe2 = pd.DataFrame(data2)
+print('Union de dataframes: ')
+print(pd.concat([dataframe1, dataframe2], axis=1)) #Union de dataframes por columnas
+
+#Union de dataframes por filas
+print('Union de dataframes por filas: ')
+dataframe3 = pd.concat([dataframe1, dataframe2], axis=0)
+print(dataframe3) #Union de dataframes por filas
+
+#Rellenar valores faltantes
+media = dataframe.mean()
+print(f'La media es igual a: {media}')
+dataframe3 = dataframe3.fillna(value=media, inplace=True)
+print('Rellenar valores faltantes: ')
+print(dataframe3)
+
+# Datos unicos
+print('Datos unicos: ')
+print(dataframe['leche'].unique()) #unique nos devuelve los valores unicos de una columna
+
+#contar cuantos valores se repiten
+print('Contar cuantos valores se repiten: ')
+print(dataframe['pan'].value_counts())
